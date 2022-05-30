5441614df01695595320631175bf3b0b66d0f720
FearMy0s <mirko.giannetta96@gmail.com>
2022-05-30 15:03:57 +0200

    push

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ const app = new Vue({
     for (let i = 0; i < 10; i++) {
       axios
         .get("https://flynn.boolean.careers/exercises/api/random/mail")
-        .then((retrive) => this.emailList.push(retrive.data.response));
+        .then((response) => this.emailList.push(response.data.response));
     }
   },
 });
